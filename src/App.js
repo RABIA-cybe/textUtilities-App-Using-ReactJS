80c8a1e6261241629f18a2b577930344633abaaf
RABIA-cybe <rabia.rpo@gmail.com>
2023-05-29 18:28:31 +0500

    Navbar darkmode enabled

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 // import logo from './logo.svg';
+import React,{ useState } from 'react';
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+
+  const [mode, setMode] = useState('light'); // for color mode of app
+
+  const toggleMode = ()=>{
+    if (mode === 'light'){
+      setMode('dark');
+    }
+    else{
+      setMode('light');
+    }
+  }
+
   return (
     <>
-      <Navbar title = "TextUtils" aboutText="About TextUtilities" />
+      <Navbar title = "TextUtils" aboutText="About TextUtilities" mode= {mode} toggleMode = {toggleMode} />
 
       <div className="container my-3">
       <TextForm heading = "Enter the text to analyze below" />
+      {/* <About /> */}
       </div>
       
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -20,17 +20,41 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
 
+     const handleClearClick = () => {
+
+        let newText = '';
+        setText(newText);
+    }    
+
+       const handleRemoveVowelsClick = () => {
+
+    const noVowels = text.replace(/[aeiou]/gi, '');
+        setText(noVowels);
+    } 
+
+    const handleCopyClick = () => {
+
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+          
+        } 
+
     const [text, setText] = useState("");
     return (
         <>
+        
         <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="10"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
-            <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
+            <button className="btn btn-success mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
+            <button className="btn btn-danger mx-1" onClick={handleRemoveVowelsClick}>Remove Vowels</button>
+            <button className="btn btn-info mx-1" onClick={handleClearClick}>Clear Text</button>
+            <button className="btn btn-warning mx-1" onClick={handleCopyClick}>Copy Text</button>
         </div>
 
         <div className="container my-3">
